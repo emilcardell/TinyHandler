a8172dc5c14cb9cfcb7ccc3271d8fde60d8936a3
Emil Cardell <emil.cardell@gmail.com>
2012-08-08 23:00:20 +0200

    Updated AssemblyInfo and NuSpec

--- Source/TinyHandler/Properties/AssemblyInfo.cs
+++ Source/TinyHandler/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TinyHandler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A tiny handler that is compositional and takes a functional aproach to handling commands, events or whatever you need handled.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Emil Cardell")]
 [assembly: AssemblyProduct("TinyHandler")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
